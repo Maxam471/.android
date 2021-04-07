638a57b7d386edc74f90b686a19aca322ea38bdb
Maxam472 <maxdrago371@gmail.com>
2021-04-07 14:25:21 +0300

    again changing with html

--- vscode-newfile/index.js
+++ vscode-newfile/index.js
@@ -1,14 +1,7 @@
-const getUser = async () => {
-  try {
-    const res = await fetch("youtube.com");
-    const data = await res.json();
-  } catch (e) {
-    console.log(e);
-  }
-};
-
-getUser();
-
 document.addEventListener("click", (event) => {
   event.preventDefault();
 });
+
+document.queryCommandEnabled("h1").style.color = "red";
+document.queryCommandEnabled("h2").style.color = "darkred";
+document.queryCommandEnabled("p").style.color = "lightcoral";
